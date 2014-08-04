5e96bfb6386882d361a5dbf44d7ee5e97856c66c
Vitor Vigano <vitorvigano@gmail.com>
2014-08-04 17:47:43 -0300

    added cross domain to the response

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/led', function (req, res) {
                 throw err;
             }
             console.log('Command ' + command + ' sent to pin ' + pin);
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             res.status(200).end();
         });
     });
