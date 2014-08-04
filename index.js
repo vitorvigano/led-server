6c830673ce950c5808d48be6ed8a1caae2a75690
Vitor Vigano <vitorvigano@gmail.com>
2014-08-04 17:26:38 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var gpio = require('rpi-gpio');
+// Get express reference.
+var app = express();
+// Map incoming calls.
+app.get('/led', function (req, res) {
+    'use strict';
+    
+    var command = req.query.command; // 0/1 == off/on.    
+    var pin = 11; // http://www.raspberrypi.org/documentation/usage/gpio/README.md
+    
+    gpio.setup(pin, gpio.DIR_OUT, function () {
+        // Send signal to Raspberry Pi.
+        gpio.write(pin, command, function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log('Command ' + command + 'Sent to pin ' + pin);
+            res.send(200);
+        });
+    });
+});
+
+// Cross domain configurations.
+app.all('*', function (req, res, next) {
+    'use strict';
+    // * == all origin.
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
+app.listen(3000);
+
+console.log('Server running at port 3000');
